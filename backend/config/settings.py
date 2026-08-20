0c68caaa0c9ee6ed9c2f706a83eb16989537a0fa
gouemoregis <gouemoregis@gmail.com>
2026-08-20 19:34:54 +0200

    Add database fallback for local CI

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,7 +85,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse('DATABASE_URL')
+        'default': dj_database_url.parse(DATABASE_URL)
     }
 else:
     DATABASES = {
